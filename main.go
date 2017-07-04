0347c8a5c97d855a90ab5d65fcde6662031db668
Ujjwal Sharma <usharma1998@gmail.com>
2017-07-04 09:48:56 +0530

    Solve challenge 11

--- main.go
+++ main.go
@@ -3,27 +3,26 @@ package main
 import "fmt"
 
 func s1() {
-  fmt.Println("Set 1")
-  c1()
-  c2()
-  c3()
-  c4()
-  c5()
-  c6()
-  c7()
-  c8()
-  fmt.Println()
+	fmt.Println("Set 1")
+	c1()
+	c2()
+	c3()
+	c4()
+	c5()
+	c6()
+	c7()
+	c8()
+	fmt.Println()
 }
 
 func s2() {
-  // fmt.Println("Set 2")
-  // c9()
-  // c10()
-  c11()
-  // fmt.Println()
+	fmt.Println("Set 2")
+	c9()
+	c10()
+	c11()
 }
 
 func main() {
-  // s1()
-  s2()
+	s1()
+	s2()
 }
